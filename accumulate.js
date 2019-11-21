6a6afff44da0906724125b0aae19edd20e1be37e
Robin De Croon <robin.decroon@cs.kuleuven.be>
2019-11-21 10:32:34 +0100

    remove strange ::before background

--- accumulate.js
+++ accumulate.js
@@ -4,7 +4,6 @@
 // show before or after time zoom (arrow?)
 // mag data online?
 // sorteren op tijd per type (mostlikelystart)
-// new document should listen to filters
 
 // reload window on resize
 $(window).resize(function() {
